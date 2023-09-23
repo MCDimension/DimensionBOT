6d982dcfa9d10db38987cb7a28d06af554c490ea
PPekKunGzDeV <kittipat25er@gmail.com>
2023-09-23 23:24:49 +0700

    main

---
+++ src/main/java/net/ppekkungz/Main.java
@@ -0,0 +1,32 @@
+package net.ppekkungz;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.ppekkungz.commands.*;
+import net.ppekkungz.commands.music.*;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+    public static void main(String[] args) throws LoginException {
+        JDA jda = JDABuilder.createDefault(Dotenv.load().get("TOKEN")).build();
+        jda.addEventListener(new Listeners());
+        CommandManager manager = new CommandManager();
+        manager.add(new Sum());
+        manager.add(new Embed());
+//        manager.add(new Buttons());
+//        manager.add(new Modals());
+//        manager.add(new Mute());
+//        manager.add(new Staff());
+//        manager.add(new UnStaff());
+//        manager.add(new Unmute());
+        manager.add(new Play());
+        manager.add(new Skip());
+        manager.add(new Stop());
+        manager.add(new NowPlaying());
+        manager.add(new Queue());
+        manager.add(new Repeat());
+        jda.addEventListener(manager);
+    }
+}
